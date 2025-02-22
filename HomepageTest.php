ad739baffde12f9da0089768f22f14b2a94d9874
iamshvan <131091310+iamshv4n@users.noreply.github.com>
2025-02-22 03:12:07 +0300

    Add files via upload

---
+++ HomepageTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Functional;
+
+class HomepageTest extends BaseTestCase
+{
+    /**
+     * Test that the index route returns a rendered response containing the text 'SlimFramework' but not a greeting
+     */
+    /*public function testGetHomepageWithoutName()
+    {
+        $response = $this->runApp('GET', '/');
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertContains('SlimFramework', (string)$response->getBody());
+        $this->assertNotContains('Hello', (string)$response->getBody());
+    }*/
+}
--- index.php
+++ index.php
@@ -1,154 +1,43 @@
-<!DOCTYPE html>
-<html lang="ku">
-    <head>
+<?php
 
-<meta name="description" content="store for plus apps ، وێبسایتێکی کوردی بۆ بەرنامە پڵاسەکان">
-<meta name="keywords" content="unc0ver, Odyssey, chmira jailbreak, Videostar++, app++, cydia, ڤیدۆستار ++، سیدیا،">
-<meta name="author" content="sardam">
-<meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="apple-mobile-web-app-capable" content="yes">
-    <meta name="apple-mobile-web-app-status-bar-style" content="white">
-    <link rel="shortcut icon" href="https://sardam12333.000webhostapp.com/icon.ico" type="image/x-icon">
-    <link rel="apple-touch-icon" href="https://sardam12333.000webhostapp.com/icon.ico">
-    <link rel="apple-touch-icon" sizes="72x72" href="https://sardam12333.000webhostapp.com/icon.ico">
-    <link rel="apple-touch-icon" sizes="114x114" href="https://sardam12333.000webhostapp.com/icon.ico">
-    <title>sardam story/سەردەم ستوری</title>
-    <meta charset="UTF-8">
+if (PHP_SAPI == 'cli-server') {
+    // To help the built-in PHP dev server, check if the request was actually for
+    // something which should probably be served as a static file
+    $url  = parse_url($_SERVER['REQUEST_URI']);
+    $file = __DIR__ . $url['path'];
+    if (is_file($file)) {
+        return false;
+    }
+}
 
+require __DIR__ . '/../vendor/autoload.php';
 
-<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet">
+session_start();
 
-<link href="dl/css/bootstrap.min.css" rel="stylesheet">
-<link href="dl/css/style.css" rel="stylesheet">
-
+// Instantiate the app
+$container = new \DI\Container();
+\Slim\Factory\AppFactory::setContainer($container);
+$app = \Slim\Factory\AppFactory::create();
+$app->addRoutingMiddleware();
+$container = $app->getContainer();
 
 
-</head><body style="background-color:#121212 ;">
+// Set up dependencies
+require __DIR__ . '/../src/dependencies.php';
 
-<link href="dl/style.css" rel="stylesheet">
+// Register middleware
+require __DIR__ . '/../src/middleware.php';
 
+/** Load routes **/
+$iterator = new RecursiveIteratorIterator(new RecursiveDirectoryIterator(realpath(__DIR__) . '/../routes'));
+$routes = array_keys(array_filter(iterator_to_array($iterator), function($file) {
+    return $file->isFile();
+}));
+foreach ($routes as $route) {
+    if (strpos($route, '.php') !== false) {
+        require_once(realpath($route));
+    }
+}
 
-<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,400i,600,700,800" rel="stylesheet">
-
-
-
-
-
-<header id="mu-header" class="" role="banner">
-<div class="mu-header-overlay">
-<div class="container">
-<div class="mu-header-area">
-
-<div class="mu-logo-area">
-
-<a class="mu-logo" href="snapchat://add/sardam-story">sardam<br>story</a>
-</div>
-
-<div class="mu-header-featured-area">
-<div class="mu-header-featured-img">
-</div>
-
-
-
-<div class="mu-header-featured-content">
-<h1>بەخیربی بو  <br> <span>سەردەم ستوری</span></h1>
-<img src="dl/img/logo.ico" alt="sardamstory">
-<p>باشترین ستورەکان بو سیستەمی ئایفون</p>
-
-     <a href="https://www.instagram.com/sardam_sadq.story/">
-<img alt="Qries" src="dl/img/ins.ico" width="190&quot;" height="190">
-<br>
-<br>
-<br>
-</a>
-<a class="mu-apple-btn"  href="/AppFiles/webclip.mobileconfig"> <i class="fa fa-apple"></i><span>دابەزاندن</span></a>
-<br>
-<br>
-<a class="mu-apple-btn" href="/app"><i class="fa fa-apple"></i><span>بێ پرۆفایل</span></a>
-</div>
-</div>
-</div>
-
-
-</div>
-</div>
-
-</header>
-
-
-<main role="main">
-</main>
-
-
-<footer id="mu-footer" role="contentinfo">
-<div class="container">
-<div class="mu-footer-area">
-<p class="mu-copy-right">© Sardam Sadq . All right reserved.</p>
-</div>
-</div>
-</footer>
-
-<!--
-<body>
-    <br><br><br>
-    <div class="stars"></div>
-    <div class="twinkling"></div>
-    <div class="galaxy"></div>
-    <div class="lamps-container">
-        <div class="lamps">
-            <img class="vert-move1" src="dl/images/2.png" />
-            <img class="vert-move3" src="dl/images/4.png" />
-            <img class="vert-move2" src="dl/images/3.png" />
-        </div>
-        <div class="lamps-sec">
-            <img class="vert-move1" src="dl/images/2.png" />
-            <img class="vert-move3" src="dl/images/4.png" />
-            <img class="vert-move2" src="dl/images/3.png" />
-        </div>
-    
-    <script>
-        // Random Stars
-        var generateStars = function () {
-
-            var $galaxy = $(".galaxy");
-            var iterator = 0;
-
-            while (iterator <= 100) {
-                var xposition = Math.random();
-                var yposition = Math.random();
-                var star_type = Math.floor((Math.random() * 3) + 1);
-                var position = {
-                    "x": $galaxy.width() * xposition,
-                    "y": $galaxy.height() * yposition,
-                };
-
-                $('<div class="star star-type' + star_type + '"></div>').appendTo($galaxy).css({
-                    "top": position.y,
-                    "left": position.x
-                });
-
-                iterator++;
-            }
-
-        };
-
-        generateStars();
-    </script>
--->
-
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-        
-
-
-<script src="/dl/js/bootstrap.min.js"></script>
-
-
-<script type="text/javascript" src="/dl/js/app.js"></script>
-
-
-<script src="dl/js/en_script.min.js"></script>
-
-
-
-</html>
+// Run app
+$app->run();
